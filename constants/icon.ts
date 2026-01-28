c54962f198e13426c211c70afd401786758f69d7
DE-IGNIS <chauhansudarsh@gmail.com>
2026-01-28 14:56:59 +0530

    Adding assets need for the project with the configuration file

---
+++ constants/icon.ts
@@ -0,0 +1,9 @@
+import sun_logo from "@/assets/icons/sun.png";
+import github_logo from "@/assets/icons/github.png";
+import search from "@/assets/icons/search.png";
+
+export const icons = {
+  sun_logo,
+  github_logo,
+  search,
+};

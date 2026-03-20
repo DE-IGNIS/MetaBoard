c98823a0d5ada812b94c0c4360400775bf977a2d
DE-IGNIS <chauhansudarsh@gmail.com>
2026-03-20 18:09:04 +0530

    pre-build optimizations

--- constants/icon.ts
+++ constants/icon.ts
@@ -1,9 +1,5 @@
-import sun_logo from "@/assets/icons/sun.png";
-import github_logo from "@/assets/icons/github.png";
 import search from "@/assets/icons/search.png";
 
 export const icons = {
-  sun_logo,
-  github_logo,
   search,
 };
